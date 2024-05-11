11ec8fe75e9d364f1025f3bb86667d50c93b23b5
kader <kaderbv@gmail.com>
2024-05-10 21:45:45 -0700

    UseRef usage

--- src/AddNewItem.js
+++ src/AddNewItem.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import {FaPlus} from 'react-icons/fa'
+import { useRef } from 'react'
 
 const AddNewItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
+
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor='addItem'>Add New Item</label>
         <input 
             id='addItem'
             type='text' 
+            ref={inputRef}
             placeholder='Add New Item' 
             value={newItem}
             onChange={(e)=>setNewItem(e.target.value)}
             required>
         </input>
         <button type='submit' 
-            aria-label='Add New Item'>
+            aria-label='Add New Item'
+            onClick={() => inputRef.current.focus()}>
             <FaPlus />
         </button>
     </form>
